9639f764f6df9bd5da7ee9004c544e912e3fc7e4
terquimocrio <diego_25mj@hotmail.com>
2022-10-06 12:12:20 -0700

    add comentarios

--- cliente/cliente.js
+++ cliente/cliente.js
@@ -2,9 +2,9 @@ const net = require("net");
 const Persona = require("../persona");
 const readline = require('readline-sync')
 const { XMLParser, XMLBuilder, XMLValidator } = require("fast-xml-parser");
-
+//el puerto a usar
 const option = {
-    port: 700,
+    port: 8002,
 };
 
 const client = net.createConnection(option);
@@ -12,11 +12,12 @@ const client = net.createConnection(option);
 const parser = new XMLParser();
 const builder = new XMLBuilder();
 
+//se conecta al servidor
 client.on("connect", () => {
     console.log("Conectado");
     sendPersona()
 });
-
+//cuando se reciben datos del servidor
 client.on("data", (data) => {
     let personaServidor = parser.parse(data);
     console.log("\n Persona del Servidor");
@@ -25,22 +26,28 @@ client.on("data", (data) => {
     console.log("Peso: " + personaServidor.peso);
     console.log("IMC: " + personaServidor.imc);
     console.log('Resultado: ' + personaServidor.resultado)
+    //envía la persona para calcular el IMC
     sendPersona()
 });
-
+//si ocurre un error
 client.on("error", (err) => {
     console.log(err.message);
 });
-
+//envía la persona al servidor
 function sendPersona(){
+    //pide los datos del cliente
     let nombre = readline.question('\n Nombre: ')
     let estatura = readline.question('\n Estatura: ')
     let peso = readline.question('\n Peso: ')
+    //crea la persona
     let personaNueva = new Persona(nombre, estatura, peso);
+    //crea un XML con los datos de la persona
     let personaXML = builder.build(personaNueva);
+    //si se desea terminar la coneción
     if (nombre == '0'){
         client.end();
     }else{
+        //envía los datos al servidor
     client.write(personaXML);}
 }
 
--- server/server.js
+++ server/server.js
@@ -6,26 +6,35 @@ const builder = new XMLBuilder
 const parser = new XMLParser();
 const server = net.createServer();
 
+//cuando hay una conecciṕn
 server.on('connection',(socket)=>{
+    //lee los datos del cliente
     socket.on('data',(data)=>{
+        //parse de xml a objeto js
         let personaCliente = parser.parse(data);
+        //se muestran los datos del cliente
         console.log('\n Persona del cliente');
         console.log(data);
         console.log('Nombre: ' + personaCliente.nombre);
         console.log('Estatura: ' + personaCliente.estatura);
         console.log('Peso: ' + personaCliente.peso);
+        //se calcula el IMC
         let imc = personaCliente.peso/personaCliente.estatura**2;
         personaCliente.imc = imc;
+        //se indica el estado de salud del cliente
         console.log('IMC: ' + personaCliente.imc);
         if (imc < 18.6){personaCliente.resultado = 'Delgado'}
         if (imc < 25 && imc > 18.6){personaCliente.resultado = 'Saludable'}
         if (imc < 30 && imc > 25){personaCliente.resultado = 'Sobrepeso'}
         if (imc > 30){personaCliente.resultado = 'Obesidad'}
+        //se imprime el resultado
         console.log('Resultado: ' + personaCliente.resultado)
+        //se crea la respuesta en XML
         let personaXML = builder.build(personaCliente)
+        //se le envía la rspuesta al cliente
         socket.write(personaXML);
     })
-
+//cuando se cierra la conección
 socket.on('close',()=>{
     console.log('Fin de comunicacion')
 })
@@ -35,7 +44,8 @@ socket.on('close',()=>{
     })
 });
 
-server.listen(700, ()=>{
+//se pone el servidor a la escucha en el puerto 8002
+server.listen(8002, ()=>{
     console.log('Servidor escuchando en el puerto ',server.address().port)
 })
 
